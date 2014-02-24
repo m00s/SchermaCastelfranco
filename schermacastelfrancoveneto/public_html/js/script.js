8dc085f6e5bf5751451bfbcefc7b419d1d5ea13f
Paolo Tesser <PaoloTesser@mbpdipaotesser2.station>
2014-02-24 21:49:12 +0100

    modifica a stile.css per parte responsive e aggiunta di uno script per la gestione del menù in script.js

---
+++ schermacastelfrancoveneto/public_html/js/script.js
@@ -0,0 +1,22 @@
+/* SCRIPT PER LA GESTIONE RESPONSIVE DEL MENU' E DEI LINK ESTERNI*/
+$(document).ready(function(){
+  $("#sezione-nav-1").click(function(){
+    $("#panel-nav-1").slideToggle("slow");
+  });
+});
+
+
+$(document).ready(function(){
+  $("#sezione-sidebar").click(function(){
+    $("#panel-sidebar").slideToggle("slow");
+  });
+});
+
+/* FINE */
+
+/* SCRIPT PER LA GESTIONE DELL'EDITOR DELL'AREA RISERVATA */
+
+
+
+
+/* FINE */
